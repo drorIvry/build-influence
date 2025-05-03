3fe2eda8e8ac6f88d083f45987ff6b358ffd222e
Dror Ivry <drorivry@gmail.com>
2025-05-03 20:01:31 +0300

    requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="build_influence",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=requirements,
+    install_requires=[
+        "litellm",
+        "mcp",
+        "loguru",
+        "pytest",
+        "typer[all]",
+        "pyyaml",
+        "python-dotenv",
+        "python-box",
+        "tqdm",
+        "rich",
+    ],
     entry_points={
         "console_scripts": [
             "build-influence=build_influence.cli:app",
